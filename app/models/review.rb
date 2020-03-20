80b747d4dffe7ee647c77a18a8db88b0191f8c03
Jeff <jeff.fisk512@gmail.com>
2020-03-20 12:30:14 -0500

    submission for deadline

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,19 @@
 class Review
+    attr_accessor :rating
+    attr_reader :customer, :restaurant
+    @@all = []
+
+    def initialize(cust, rest, rat)
+        @customer = cust
+        @restaurant = rest
+        @rating = rat
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    
   
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+jeff = Customer.new("Jeff", "Fisk")
+tim = Customer.new("Tim", "Jones")
+linda = Customer.new("Linda", "Samson")
+jill = Customer.new("Jill", "McIntosh")
+
+chuys = Restaurant.new("Chuy's")
+dos = Restaurant.new("Dos Salsas")
+
+# yes, our stars go to 10 around here
+jeff_dos = Review.new(jeff, dos, 10)
+jeff_chuys = Review.new(jeff, chuys, 6)
+tim_dos = Review.new(tim, dos, rand(10))
+tim_chuys = Review.new(tim, chuys, rand(10))
+linda_dos = Review.new(linda, dos, rand(10))
+linda_chuys = Review.new(linda, chuys, rand(10))
+jill_dos = Review.new(jill, dos, rand(10))
+jill_chuys = Review.new(jill, chuys, rand(10))
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
